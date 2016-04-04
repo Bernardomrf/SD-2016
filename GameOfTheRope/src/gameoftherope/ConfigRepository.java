272fc4bf48ed4633b63bfc2ece479d453873c430
Bruno Silva <brunomiguelsilva@ua.pt>
2016-04-04 14:05:48 +0100

    added ConfigRepository with config for all entities and regions

---
+++ GameOfTheRope/src/gameoftherope/ConfigRepository.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameoftherope;
+
+/**
+ *
+ * @author Bruno Silva <brunomiguelsilva@ua.pt>
+ */
+public class ConfigRepository {
+    
+    // Player Configs
+    private final int maxStrength = 4;
+    
+    // Ref Configs
+    private final int nTrials = 6;
+    private final int nGames = 3;
+   
+    // Bench Configs
+    private final int nTeamPlayers = 5;
+    private final int nTrialPlayers = 3;
+    
+    // Playground Configs
+    private final int totalTrialPlayers = nTrialPlayers * 2;
+    private final int knockOutForce = 4;
+    private final int nTrialsOfGameDefault = 6;
+    
+    // Main Configs
+    private final int nCoaches = 2;
+    private final int nPlayers = 10;
+    
+    
+    
+}

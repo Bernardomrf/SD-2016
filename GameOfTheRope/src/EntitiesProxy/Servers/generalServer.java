4c412c394c3b52278695421962473988fc552e4e
Bruno Silva <brunomiguelsilva@ua.pt>
2016-04-06 17:29:16 +0100

    started to create general server with server socket
    Created game of the rope protocol to help define which handler to use

--- GameOfTheRope/src/EntitiesProxy/Servers/generalServer.java
+++ GameOfTheRope/src/EntitiesProxy/Servers/generalServer.java
@@ -5,10 +5,18 @@
  */
 package EntitiesProxy.Servers;
 
+import java.net.ServerSocket;
+
 /**
  *
  * @author Bruno Silva <brunomiguelsilva@ua.pt>
  */
 public class generalServer {
+    
+    public static void main(String[] args) {
+        ServerSocket listeningSocket = null;           // socket de escuta
+        int portNumb = 4000;                           // número do port em que o serviço é estabelecido
+        GameOfTheRopeProtocol prot = new GameOfTheRopeProtocol();
+    }
     
 }

bb13f4aa749b94d3c63b136fc9b77355d3950398
Bruno Silva <brunomiguelsilva@ua.pt>
2016-04-18 14:01:37 +0100

    changed files

--- GameOfTheRope/src/gameoftherope/Protocols/RefSiteProtocol.java
+++ GameOfTheRope/src/gameoftherope/Protocols/RefSiteProtocol.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gameoftherope;
+package gameoftherope.Protocols;
 
 /**
  *
  * @author Bruno Silva <brunomiguelsilva@ua.pt>
  */
-public class GameOfTheRopeProtocol {
-    // Definir que handler usar dependendo do serviço que se connectar
+public class RefSiteProtocol {
+    
 }

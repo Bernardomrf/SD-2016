0ac1006e7f7b4ac9a083ac7685fc5c28b8da3115
Bruno Silva <brunomiguelsilva@ua.pt>
2016-04-24 14:08:19 +0100

    added files for config

---
+++ GameOfTheRope/src/gameoftherope/Regions/Configs/PlayerConfig.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameoftherope.Regions.Configs;
+
+/**
+ *
+ * @author Bruno Silva <brunomiguelsilva@ua.pt>
+ */
+public class PlayerConfig {
+    private int maxStrength = 4;
+
+    public int getMaxStrength() {
+        return maxStrength;
+    }
+    
+    public void setMaxStrength(int max) {
+        maxStrength = max;
+    }
+}
---
+++ GameOfTheRope/src/gameoftherope/Regions/Configs/RefConfig.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameoftherope.Regions.Configs;
+
+/**
+ *
+ * @author Bruno Silva <brunomiguelsilva@ua.pt>
+ */
+public class RefConfig {
+    private int nTrials = 6;
+    private int nGames = 3;
+
+    public int getNTrials() {
+        return nTrials;
+    }
+    
+    public void setNTrials(int n) {
+        nTrials = n;
+    }
+    
+    public int getNGames() {
+        return nGames;
+    }
+    
+    public void setNGames(int n) {
+        nGames = n;
+    }
+}
